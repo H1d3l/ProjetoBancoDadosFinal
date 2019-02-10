1753bbb4680445b4341ac28c91b69ed9c732b3ff
H1d3l <hidels.rios@gmail.com>
2019-02-10 11:03:29 -0200

    funcionalidade ordem de servico

--- Empreguetes/script empreguete.sql
+++ Empreguetes/script empreguete.sql
@@ -10,7 +10,7 @@ CREATE TABLE CLIENTE
   ID_CLIENTE           SERIAL UNIQUE PRIMARY KEY,
   NOME_CLIENTE         VARCHAR(200) NOT NULL,
   TELEFONE             VARCHAR(30)  NOT NULL,
-  ID_CATEGORIA_CLIENTE INT REFERENCES CATEGORIA_CLIENTE (ID_CATEGORIA)
+  CATEGORIA_CLIENTE INT REFERENCES CATEGORIA_CLIENTE (ID_CATEGORIA)
 
 );
 
@@ -38,31 +38,33 @@ CREATE TABLE SERVICOS
 
 );
 
-CREATE TABLE SOLICITACAO
+CREATE TABLE ORDEM_DE_SERVICO
 (
-  ID_SOLICITACAO    SERIAL UNIQUE PRIMARY KEY,
-  ID_CLIENTE        INT REFERENCES CLIENTE (ID_CLIENTE),
-  ID_FUNCIONARIO    INT REFERENCES FUNCIONARIO (ID_FUNCIONARIO),
+  ID_ORDEM_DE_SERVICO    SERIAL UNIQUE PRIMARY KEY,
+  CLIENTE        INT REFERENCES CLIENTE (ID_CLIENTE),
+  FUNCIONARIO    INT REFERENCES FUNCIONARIO (ID_FUNCIONARIO),
+  DIARISTA INT REFERENCES DIARISTA(ID_DIARISTA),
   DATA              DATE  NOT NULL,
   HORA              TIME  NOT NULL,
-  VALOR_SOLICITACAO FLOAT NOT NULL
+  SERVICO INT REFERENCES SERVICOS(ID_SERVICO),
+  VALOR_TOTAL FLOAT NOT NULL
 );
 
+CREATE TABLE ITEM_ORDEM_DE_SERVICO
+(
+  ID_ITEM_ORDEM_DE_SERVICO SERIAL UNIQUE PRIMARY KEY,
+  ORDEM_DE_SERVICO INT REFERENCES ORDEM_DE_SERVICO(ID_ORDEM_DE_SERVICO),
+  SERVICO INT REFERENCES SERVICOS(ID_SERVICO),
+  VALOR INT NOT NULL
+
+);
 CREATE TABLE DIARISTA_SERVICO
 (
   ID_DIARISTA INT REFERENCES DIARISTA (ID_DIARISTA),
   ID_SERVICO  INT REFERENCES SERVICOS (ID_SERVICO)
 );
 
-CREATE TABLE SOLICITACAO_SERVICO
-(
-  ID_SOLICITACAO       INT REFERENCES SOLICITACAO (ID_SOLICITACAO),
-  ID_CLIENTE           INT REFERENCES CLIENTE (ID_CLIENTE),
-  ID_CATEGORIA_CLIENTE INT REFERENCES CATEGORIA_CLIENTE (ID_CATEGORIA),
-  ID_FUNCIONARIO       INT REFERENCES FUNCIONARIO (ID_FUNCIONARIO),
-  ID_SERVICO           INT REFERENCES SERVICOS (ID_SERVICO),
-  ID_DIARISTA          INT REFERENCES DIARISTA (ID_DIARISTA)
-);
+
 ---------------------------------------------------Povoamento-----------------------------------------------------------
 
 INSERT INTO CATEGORIA_CLIENTE(NOME_CATEGORIA, DESCONTO)
@@ -72,30 +74,30 @@ VALUES ('PRATA', 0.3);
 INSERT INTO CATEGORIA_CLIENTE(NOME_CATEGORIA, DESCONTO)
 VALUES ('OURO', 0.5);
 
-INSERT INTO CLIENTE(NOME_CLIENTE, TELEFONE, ID_CATEGORIA_CLIENTE)
+INSERT INTO CLIENTE(NOME_CLIENTE, TELEFONE, CATEGORIA_CLIENTE)
 VALUES ('JOÃO', '998500295', 1);
-INSERT INTO CLIENTE(NOME_CLIENTE, TELEFONE, ID_CATEGORIA_CLIENTE)
+INSERT INTO CLIENTE(NOME_CLIENTE, TELEFONE, CATEGORIA_CLIENTE)
 VALUES ('PEDRO', '998657901', 2);
-INSERT INTO CLIENTE(NOME_CLIENTE, TELEFONE, ID_CATEGORIA_CLIENTE)
+INSERT INTO CLIENTE(NOME_CLIENTE, TELEFONE, CATEGORIA_CLIENTE)
 VALUES ('RICARDO', '5132715375', 3);
-INSERT INTO CLIENTE(NOME_CLIENTE, TELEFONE, ID_CATEGORIA_CLIENTE)
+INSERT INTO CLIENTE(NOME_CLIENTE, TELEFONE, CATEGORIA_CLIENTE)
 VALUES ('CONCEIÇÃO', '1231242615', 2);
-INSERT INTO CLIENTE(NOME_CLIENTE, TELEFONE, ID_CATEGORIA_CLIENTE)
+INSERT INTO CLIENTE(NOME_CLIENTE, TELEFONE, CATEGORIA_CLIENTE)
 VALUES ('EDSON', '1312213333', 3);
-INSERT INTO CLIENTE(NOME_CLIENTE, TELEFONE, ID_CATEGORIA_CLIENTE)
+INSERT INTO CLIENTE(NOME_CLIENTE, TELEFONE, CATEGORIA_CLIENTE)
 VALUES ('PAULO', '145345552', 1);
 
 INSERT INTO FUNCIONARIO(NOME_FUNCIONARIO, ENDERECO, TELEFONE)
 VALUES ('', 'AVENIDA MIGUEL ROSA', '8798787361');
-INSERT INTO FUNCIONARIO(NOME_FUNCIONARIO, ENDEREÇO, TELEFONE)
+INSERT INTO FUNCIONARIO(NOME_FUNCIONARIO, ENDERECO, TELEFONE)
 VALUES ('PABLO', 'AVENIDA FREI SERAFIM', '87876311312');
-INSERT INTO FUNCIONARIO(NOME_FUNCIONARIO, ENDEREÇO, TELEFONE)
+INSERT INTO FUNCIONARIO(NOME_FUNCIONARIO, ENDERECO, TELEFONE)
 VALUES ('CHICO', 'RUA TERESINHA', '9876546889');
-INSERT INTO FUNCIONARIO(NOME_FUNCIONARIO, ENDEREÇO, TELEFONE)
+INSERT INTO FUNCIONARIO(NOME_FUNCIONARIO, ENDERECO, TELEFONE)
 VALUES ('WANESSA', 'AVENIDA PIREZ DE CASTRO', '9897909876');
-INSERT INTO FUNCIONARIO(NOME_FUNCIONARIO, ENDEREÇO, TELEFONE)
+INSERT INTO FUNCIONARIO(NOME_FUNCIONARIO, ENDERECO, TELEFONE)
 VALUES ('DEBORA', 'RUA 100', '8798731132');
-INSERT INTO FUNCIONARIO(NOME_FUNCIONARIO, ENDEREÇO, TELEFONE)
+INSERT INTO FUNCIONARIO(NOME_FUNCIONARIO, ENDERECO, TELEFONE)
 VALUES ('JUVENAL', 'RUA 90', '9876588990');
 
 INSERT INTO DIARISTA(NOME_DIARISTA, ENDERECO, TELEFONE)
@@ -109,7 +111,11 @@ VALUES ('JESSICA', 'BAIRRO HORTO', '445323113');
 INSERT INTO DIARISTA(NOME_DIARISTA, ENDERECO, TELEFONE)
 VALUES ('BRUNO', 'BAIRRO DIRCEU', '123123877');
 
-
+INSERT INTO SERVICOS VALUES (DEFAULT,'VARRER ',20.00);
+INSERT INTO SERVICOS VALUES (DEFAULT,'LAVAR',20.00);
+INSERT INTO SERVICOS VALUES (DEFAULT,'COZINHAR',30.00);
+INSERT INTO SERVICOS VALUES (DEFAULT,'PASSAR',50.00);
+INSERT INTO SERVICOS VALUES (DEFAULT,'SERVICO COMPLETO',110.00);
 
 -------------------------------------------------- Funções -------------------------------------------------------------
   ---> Inserir
@@ -127,11 +133,12 @@ BEGIN
 END;
 
 $$ LANGUAGE plpgsql;
-
+------------------------------------------------------------------------------------------------------------------------
 select inserir('funcionario', ''''' ,''rua 18'',''9886788776''');
-
+------------------------------------------------------------------------------------------------------------------------
 --->Update
 drop function atualizar(tabela text, campo text, valorantigo text);
+------------------------------------------------------------------------------------------------------------------------
 create or replace function atualizar(tabela text, campo text,valornovo text,condicao text,valor_condicao text)
   returns void as
 $$
@@ -145,10 +152,9 @@ begin
 
 end;
 $$ language plpgsql;
-
+------------------------------------------------------------------------------------------------------------------------
 select atualizar('funcionario','nome_funcionario','''junior''','id_funcionario','5');
-
-
+------------------------------------------------------------------------------------------------------------------------
 --->Delete
 
 create or replace function deletar(tabela text,campo text,valor text)
@@ -161,9 +167,32 @@ returns void as $$
     execute comando;
   end;
   $$ language plpgsql;
+------------------------------------------------------------------------------------------------------------------------
+-----Criar ordem de servico
 
+create or replace function criar_ordem_de_servico(NOME_CLIENTE_V varchar(100),NOME_FUNCIONARIO_V varchar(100),
+NOME_DIARISTA_V varchar(100),DATA date,HORA time,NOME_SERVICO_V VARCHAR(100)) returns void as $$
+  declare
+    var_id_cliente int;
+    var_id_funcionario int;
+    var_id_diarista int;
+    var_id_servico int;
+    var_valor_servico float;
 
+  begin
 
+    SELECT ID_CLIENTE INTO var_id_cliente FROM CLIENTE WHERE NOME_CLIENTE_V ILIKE NOME_CLIENTE;
+    SELECT ID_FUNCIONARIO INTO var_id_funcionario FROM FUNCIONARIO WHERE NOME_FUNCIONARIO_V ILIKE NOME_FUNCIONARIO;
+    SELECT ID_DIARISTA INTO var_id_diarista FROM DIARISTA WHERE NOME_DIARISTA_V ILIKE NOME_DIARISTA;
+    SELECT ID_SERVICO,VALOR_SERVICO INTO var_id_servico,var_valor_servico FROM SERVICOS WHERE NOME_SERVICO_V ILIKE NOME_SERVICO;
+
+    INSERT INTO ORDEM_DE_SERVICO VALUES (DEFAULT,var_id_cliente,var_id_funcionario,var_id_diarista,DATA,HORA,var_id_servico,
+                                         var_valor_servico);
+
+  end;
+  $$ LANGUAGE plpgsql;
+
+select criar_ordem_de_servico('edson','pablo','maria','12-12-2018','12:30','COZINHAR');
 ---------------------------------------------Trigger--------------------------------------------------------------------
 ----- feedback
 CREATE TRIGGER TGR_FEEDBACK_OPERACAO
@@ -172,12 +201,8 @@ CREATE TRIGGER TGR_FEEDBACK_OPERACAO
   ON funcionario
   FOR EACH ROW EXECUTE PROCEDURE FEEDBACK_OPERACAO();
 
-DROP FUNCTION FEEDBACK_OPERACAO() CASCADE;
-
-CREATE OR REPLACE FUNCTION FEEDBACK_OPERACAO()
-  RETURNS TRIGGER AS
+CREATE OR REPLACE FUNCTION FEEDBACK_OPERACAO() RETURNS TRIGGER AS
 $$
-
 DECLARE
 BEGIN
   IF (TG_OP = 'INSERT')
@@ -198,13 +223,13 @@ $$
 LANGUAGE plpgsql;
 
 
-----------
-
+------------------------------------------------------------------------------------------------------------------------
+-------TRIGGER NAO ACEITA VALORES NULOS OU VAZIOS
 create or replace function notvaluesnull()
 returns trigger as $$
   begin
-    if new.nome_funcionario  is null  or new.NOME_FUNCIONARIO = '' then
-    raise exception 'nome nulo';
+    if new is null  or new = '' then
+    raise exception 'CAMPO COM VALOR NULO OU VAZIO';
     end if;
     return null;
   end;
@@ -212,7 +237,6 @@ returns trigger as $$
   $$language plpgsql;
 
 create  trigger notnullfunc before insert or update on funcionario  FOR EACH ROW EXECUTE PROCEDURE notvaluesnull();
-drop trigger notnullfunc on funcionario cascade ;
 ----------------------------------------------teste função inserir------------------------------------------------------
 select inserir('funcionario', ''''' ,''rua 18'',''98867887731231''');
 select inserir('categoria_cliente', '''platina3'',''90''');
@@ -223,6 +247,5 @@ select inserir('servicos', '''lavar roupa'',''25''');
 insert into FUNCIONARIO values (default,'','rua20','65767565765');
 
 select deletar('funcionario','nome_funcionario','');
+------------------------------------------------------------------------------------------------------------------------
 
-
-select * from funcionario
